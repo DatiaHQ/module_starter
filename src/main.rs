cde5a18782cfd2fdcf752809d48341398a0885a9
nuzzesick <mati280341@gmail.com>
2024-02-10 13:38:14 +0100

    chore: creating router, privateRoute and components folder

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,22 @@ fn main() {
     let dir = Path::new(&core_repo_path).join("src").join("views").join("fund-manager").join(module_name);
     if !dir.exists() {
         fs::create_dir_all(&dir).expect("Error when creating directory");
+        println!("Directory created: {:?}", dir);
     }
 
-    let file_path = dir.join("index.js");
-    fs::write(file_path, "console.log('Module created!');\n").expect("Error when writing file");
+    let router_file_path = dir.join(format!("{}Router.js", module_name));
+    fs::write(&router_file_path, "console.log('Router created!');\n").expect("Error when writing file");
+    println!("File created: {:?}", router_file_path);
+
+    let private_route_file_path = dir.join(format!("{}PrivateRoute.js", module_name));
+    fs::write(&private_route_file_path, "console.log('PrivateRoute created!');\n").expect("Error when writing file");
+    println!("File created: {:?}", private_route_file_path);
+
+    let components_dir = dir.join("components");
+    fs::create_dir_all(&components_dir).expect("Error when creating directory");
+    println!("Directory created: {:?}", components_dir);
+
+    let components_index_file_path = components_dir.join("index.js");
+    fs::write(&components_index_file_path, "").expect("Error when writing file");
+    println!("File created: {:?}", components_index_file_path);
 }
