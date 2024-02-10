62bcc5b0fa56ac1642e7c242f11d65e7d079b705
nuzzesick <mati280341@gmail.com>
2024-02-10 12:51:59 +0100

    chore: creating module inside src/views/fund-manager

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,21 @@ use std::env;
 use dotenv;
 
 fn main() {
-	dotenv::from_filename(".env").ok();
+    dotenv::from_filename(".env").ok();
 
-	let core_repo_path = env::var("CORE_REPO_PATH").expect("CORE_REPO_PATH is not defined");
+    let core_repo_path = env::var("CORE_REPO_PATH").expect("CORE_REPO_PATH is not defined");
 
-	let mut module_name = String::new();
-	print!("Type the module name (e.g: TemperatureScore): ");
-	let _= stdout().flush();
-	stdin().read_line(&mut module_name).expect("Error when reading the line");
-	let module_name = module_name.trim();
+    let mut module_name = String::new();
+    print!("Type the module name (e.g: TemperatureScore): ");
+    let _= stdout().flush();
+    stdin().read_line(&mut module_name).expect("Error when reading the line");
+    let module_name = module_name.trim();
 
-	let dir = Path::new(&core_repo_path).join(module_name);
-	if !dir.exists() {
-		fs::create_dir_all(&dir).expect("Error when creating directory");
-	}
+    let dir = Path::new(&core_repo_path).join("src").join("views").join("fund-manager").join(module_name);
+    if !dir.exists() {
+        fs::create_dir_all(&dir).expect("Error when creating directory");
+    }
 
-	let file_path = dir.join("index.js");
-	fs::write(file_path, "console.log('Module created!');\n").expect("Error when writing file");
+    let file_path = dir.join("index.js");
+    fs::write(file_path, "console.log('Module created!');\n").expect("Error when writing file");
 }
