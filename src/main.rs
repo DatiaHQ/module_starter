c531c7497fc544fc9031e956995881ec7a7fa272
nuzzesick <mati280341@gmail.com>
2024-02-19 02:46:28 +0100

    chore: created methods to avoid spaghetti code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::io::{stdin, stdout, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::env;
 use dotenv;
 mod templates {
@@ -9,61 +9,74 @@ mod templates {
 }
 
 fn main() {
-    dotenv::from_filename(".env").ok();
+  dotenv::from_filename(".env").ok();
 
-    let core_repo_path = env::var("CORE_REPO_PATH").expect("CORE_REPO_PATH is not defined");
+  let core_repo_path = env::var("CORE_REPO_PATH").expect("CORE_REPO_PATH is not defined");
 
-    let mut module_name = String::new();
-    print!("Type the module name (e.g: TemperatureScore): ");
-    let _= stdout().flush();
-    stdin().read_line(&mut module_name).expect("Error when reading the line");
-    let module_name = module_name.trim();
+  let module_name = get_module_name();
 
-    let dir = Path::new(&core_repo_path).join("src").join("views").join("fund-manager").join(module_name);
+  let dir = create_directory(&core_repo_path, &module_name);
 
-    if !dir.exists() {
-        fs::create_dir_all(&dir).expect("Error when creating directory");
-        println!("Directory created: {:?}", dir);
-    }
+  create_router_file(&dir, &module_name);
+  create_private_route_file(&dir, &module_name);
 
-    let router_file_path = dir.join(format!("{}Router.js", module_name));
+  let components_dir = create_sub_directory(&dir, "components");
+  create_file(&components_dir, "index.js", "");
 
-    let router_file_content = templates::router::get_router_file_content(module_name);
+  let views_dir = create_sub_directory(&dir, "views");
+  create_file(&views_dir, "index.js", &format!("export {{ default as {}Home }} from \"./{}Home\";\n", module_name, module_name));
 
-    fs::write(&router_file_path, router_file_content).expect("Error when writing file");
+  let home_dir = create_sub_directory(&views_dir, &format!("{}Home", module_name));
+  create_file(&home_dir, "index.js", &format!("export {{ default }} from \"./{}Home\";\n", module_name));
+  create_home_file(&home_dir, &module_name);
+}
 
-    println!("File created: {:?}", router_file_path);
+fn get_module_name() -> String {
+  let mut module_name = String::new();
+  print!("Type the module name (e.g: TemperatureScore): ");
+  let _= stdout().flush();
+  stdin().read_line(&mut module_name).expect("Error when reading the line");
+  module_name.trim().to_string()
+}
 
-    let private_route_file_path = dir.join(format!("{}PrivateRoute.js", module_name));
-    fs::write(&private_route_file_path, "console.log('PrivateRoute created!');\n").expect("Error when writing file");
-    println!("File created: {:?}", private_route_file_path);
+fn create_directory(core_repo_path: &str, module_name: &str) -> PathBuf {
+  let dir = Path::new(core_repo_path).join("src").join("views").join("fund-manager").join(module_name);
+  if !dir.exists() {
+    fs::create_dir_all(&dir).expect("Error when creating directory");
+    println!("Directory created: {:?}", dir);
+  }
+  dir
+}
 
-    let components_dir = dir.join("components");
-    fs::create_dir_all(&components_dir).expect("Error when creating directory");
-    println!("Directory created: {:?}", components_dir);
+fn create_router_file(dir: &Path, module_name: &str) {
+  let router_file_path = dir.join(format!("{}Router.js", module_name));
+  let router_file_content = templates::router::get_router_file_content(module_name);
+  fs::write(&router_file_path, router_file_content).expect("Error when writing file");
+  println!("File created: {:?}", router_file_path);
+}
 
-    let components_index_file_path = components_dir.join("index.js");
-    fs::write(&components_index_file_path, "").expect("Error when writing file");
-    println!("File created: {:?}", components_index_file_path);
+fn create_private_route_file(dir: &Path, module_name: &str) {
+  let private_route_file_path = dir.join(format!("{}PrivateRoute.js", module_name));
+  fs::write(&private_route_file_path, "console.log('PrivateRoute created!');\n").expect("Error when writing file");
+  println!("File created: {:?}", private_route_file_path);
+}
 
-    let views_dir = dir.join("views");
-    fs::create_dir_all(&views_dir).expect("Error when creating directory");
-    println!("Directory created: {:?}", views_dir);
+fn create_sub_directory(dir: &Path, sub_dir_name: &str) -> PathBuf {
+  let sub_dir = dir.join(sub_dir_name);
+  fs::create_dir_all(&sub_dir).expect("Error when creating directory");
+  println!("Directory created: {:?}", sub_dir);
+  sub_dir
+}
 
-    let views_index_file_path = views_dir.join("index.js");
-    fs::write(&views_index_file_path, format!("export {{ default as {}Home }} from \"./{}Home\";\n", module_name, module_name)).expect("Error when writing file");
-    println!("File created: {:?}", views_index_file_path);
+fn create_file(dir: &Path, file_name: &str, content: &str) {
+  let file_path = dir.join(file_name);
+  fs::write(&file_path, content).expect("Error when writing file");
+  println!("File created: {:?}", file_path);
+}
 
-    let home_dir = views_dir.join(format!("{}Home", module_name));
-    fs::create_dir_all(&home_dir).expect("Error when creating directory");
-    println!("Directory created: {:?}", home_dir);
-
-    let home_index_file_path = home_dir.join("index.js");
-    fs::write(&home_index_file_path, format!("export {{ default }} from \"./{}Home\";\n", module_name)).expect("Error when writing file");
-    println!("File created: {:?}", home_index_file_path);
-
-    let home_file_path = home_dir.join(format!("{}Home.js", module_name));
-    let home_file_content = templates::home::get_home_file_content(module_name);
-    fs::write(&home_file_path, home_file_content).expect("Error when writing file");
-    println!("File created: {:?}", home_file_path);
+fn create_home_file(dir: &Path, module_name: &str) {
+  let home_file_path = dir.join(format!("{}Home.js", module_name));
+  let home_file_content = templates::home::get_home_file_content(module_name);
+  fs::write(&home_file_path, home_file_content).expect("Error when writing file");
+  println!("File created: {:?}", home_file_path);
 }
